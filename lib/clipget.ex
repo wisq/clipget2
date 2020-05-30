1aeb39f28e2fa27d707e27482bd977fedab9008c
Adrian Irving-Beer <wisq@wisq.net>
2020-05-30 18:23:34 -0400

    `mix new clipget`

---
+++ lib/clipget.ex
@@ -0,0 +1,18 @@
+defmodule Clipget do
+  @moduledoc """
+  Documentation for `Clipget`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clipget.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clipget_test.exs
@@ -0,0 +1,8 @@
+defmodule ClipgetTest do
+  use ExUnit.Case
+  doctest Clipget
+
+  test "greets the world" do
+    assert Clipget.hello() == :world
+  end
+end
